6246f60baa60163d42488137141e6e769960bd01
Güven Altunsoy <guven@shopihq.com>
2019-12-31 10:50:04 +0300

    added empty container list

--- app/src/main/java/com/example/iot/CustomAdapter.java
+++ app/src/main/java/com/example/iot/CustomAdapter.java
@@ -13,12 +13,14 @@ import java.util.ArrayList;
 public class CustomAdapter extends BaseAdapter {
     Context context;
     ArrayList<Container> Containers;
+    boolean isEmpty;
     String BASE_URL = "https://2bw3s8zet7.execute-api.eu-west-1.amazonaws.com/dev/?filled=5e07475fe09e688c3473fdca";
     private static LayoutInflater inflater = null;
 
-    public CustomAdapter(Context context, ArrayList<Container> Containers) {
+    public CustomAdapter(Context context, ArrayList<Container> Containers, boolean isEmpty) {
         // TODO Auto-generated constructor stub
         this.context = context;
+        this.isEmpty = isEmpty;
         this.Containers = Containers;
         inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -48,12 +50,14 @@ public class CustomAdapter extends BaseAdapter {
         View vi = convertView;
         if (vi == null)
             vi = inflater.inflate(R.layout.row, null);
-        TextView address, lat, lon;
+        TextView address, lat, lon, sum;
         Button btnSum;
         address = (TextView) vi.findViewById(R.id.address);
         lat = (TextView) vi.findViewById(R.id.lat);
         lon = (TextView) vi.findViewById(R.id.lon);
         address.setText(Containers.get(position).getAddress());
+        sum = vi.findViewById(R.id.sum);
+        sum.setText(isEmpty ? "Dolu" : "Toplandı");
         lat.setText(String.valueOf(Containers.get(position).getLat()));
         lon.setText(String.valueOf(Containers.get(position).getLon()));
         return vi;
--- app/src/main/java/com/example/iot/MainActivity.java
+++ app/src/main/java/com/example/iot/MainActivity.java
@@ -21,12 +21,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
-
 
 public class MainActivity extends AppCompatActivity {
     public AsyncHttpClient asyncHttpClient;
     public ArrayList<Container> Containers;
+    public ArrayList<Container> CompletedContainers;
     public String BASE_URL = "https://2bw3s8zet7.execute-api.eu-west-1.amazonaws.com/dev/";
     ListView listView;
     Container container;
@@ -38,13 +37,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button clickMe = findViewById(R.id.btnClickMe);
+        Button btnEmptyContainers = findViewById(R.id.btnEmptyContainer);
         listView = findViewById(R.id.listView);
         Containers = new ArrayList<Container>();
+        CompletedContainers = new ArrayList<Container>();
         getList();
         clickMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getList();
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
+                final String container_id = Containers.get(position).getId();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(container_id);
+                builder.setMessage(container_id + " id'li konteynerı topladınız mı?");
+                builder.setNegativeButton("Hayır", null);
+                builder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Containers.remove(position);
+                        setList();
+                        CollectGarbage(container_id);
+                    }
+                });
+                builder.show();
+            }
+        });
+        btnEmptyContainers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getEmptyContainerList();
             }
         });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -72,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         Containers.clear();
         try {
             asyncHttpClient = new AsyncHttpClient();
-            asyncHttpClient.get(BASE_URL + "?region=izmit", new AsyncHttpResponseHandler() {
+            asyncHttpClient.get(BASE_URL + "?region=izmit&lat=40.762844&lon=29.925699", new AsyncHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
                     String response = new String(responseBody);
@@ -105,17 +131,62 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    public void getEmptyContainerList() {
+        CompletedContainers.clear();
+        try {
+            asyncHttpClient = new AsyncHttpClient();
+            asyncHttpClient.get(BASE_URL + "?completedWithRegion=izmit", new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
+                    String response = new String(responseBody);
+                    JSONArray jsonArr = null;
+                    try {
+                        jsonArr = new JSONArray(response);
+                        for (int i = 0; i < jsonArr.length(); i++) {
+                            JSONObject jsonObj = jsonArr.getJSONObject(i);
+                            System.out.println(jsonObj);
+                            container = new Container(jsonObj.getString("id"), jsonObj.getDouble("lat"), jsonObj.getDouble("lon"), jsonObj.getInt("order"), jsonObj.getString("address"), jsonObj.getBoolean("isFilled"));
+                            CompletedContainers.add(container);
+                        }
+                        for (Container con :
+                                CompletedContainers) {
+                            System.out.println(con.getId());
+                        }
+                        setEmptyList();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
+
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void setList() {
         try {
-            listView.setAdapter(new CustomAdapter(this, Containers));
+            listView.setAdapter(new CustomAdapter(this, Containers, true));
         } catch (Exception ex) {
             System.out.println(ex);
         }
         System.out.println("setList()");
         System.out.println("Container size: " + Containers.size());
     }
-
+    public void setEmptyList() {
+        try {
+            listView.setAdapter(new CustomAdapter(this, CompletedContainers, false));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        System.out.println("setList()");
+        System.out.println("Completed Container size: " + CompletedContainers.size());
+    }
     public void CollectGarbage(String id) {
         try {
             asyncHttpClient = new AsyncHttpClient();
